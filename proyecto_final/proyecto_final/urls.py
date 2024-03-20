c347fbc470ce7d8353bba96c5c4588bdd1bc0b07
Matias Vazquez <mativazquezfotografia@hotmail.com>
2024-03-19 22:09:54 -0300

    news urls

--- proyecto_final/proyecto_final/urls.py
+++ proyecto_final/proyecto_final/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.http import HttpResponse
+
+
+def mi_func(xx):
+    return HttpResponse("<h1>Bienvenidos a mi Proyecto</h1>")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", mi_func),
+    path("bookings/", include("final_python.urls")), # conecto las URLS de `bookings` con las URLS generales
 ]
